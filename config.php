bb807155993692d33cd1a31f95a0ae33cdf26140
Sankalp <sankalpofficial16@gmail.com>
2021-06-20 22:15:55 +0530

    DB Name Changed

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-	$conn = mysqli_connect('localhost','root',"","sankalp");
+	$conn = mysqli_connect('localhost','root',"","bankingsystem");
 
 	if(!$conn){
 		die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
